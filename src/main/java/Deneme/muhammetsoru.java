f118744c942c85e76d8a086d49d76176c228069b
Muhammetckmk <muhammetckmk46@gmail.com>
2024-09-22 10:41:28 +0300

    Update muhammetsoru.java

--- src/main/java/Deneme/muhammetsoru.java
+++ src/main/java/Deneme/muhammetsoru.java
@@ -9,6 +9,7 @@ public class muhammetsoru {
  Kullanıcıdan 5 adet sayı isteyiniz
  Bu sayılardan 5 ile 10 arasındakiler hariç, diğerlerinin toplamını bulunuz.
  */
+        bu soruyu çözemedim
 
 
     }

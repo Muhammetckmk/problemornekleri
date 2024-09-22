72a2db6a9dc1e0f15882bc4cfc7daa10cf0f62fe
Muhammet Çakmak <muhammetckmk46@gmail.com>
2024-09-22 15:41:46 +0300

    muhammetsoru2

--- src/main/java/Deneme/fsdf.java
+++ src/main/java/Deneme/fsdf.java
@@ -6,9 +6,23 @@ public class fsdf {
     public static void main(String[] args) {
 
           Scanner input = new Scanner(System.in);
+        System.out.println("zikir değişikliğinizi giriniz \n arttırma :+ \n azaltmak : -");
+
+
+          int toplam =0;
 
         for (int i = 0; i <input.nextInt() ; i++) {
-            System.out.println(i + ". zikir");
+            char sembol = input.next().charAt(0);
+            switch (sembol){
+                case '+':
+                    System.out.println((toplam + i ) + ". zikir");
+                    break;
+                case'-':
+                    System.out.println((toplam - i )+ ". zikir");
+                    break;
+
+            }
+
         }
         }
 
---
+++ src/main/java/Deneme/renk.java
@@ -0,0 +1,45 @@
+package Deneme;
+
+import java.util.Scanner;
+
+public class renk {
+    public static void main(String[] args) {
+              /*  1. Renk Karışımı
+                Bir program yazın, kullanıcı iki temel renk seçsin (Kırmızı, Mavi, Sarı) ve program bu iki rengin karışımını ekrana yazsın.
+                Örneğin, kırmızı ve mavi karıştığında mor, kırmızı ve sarı karıştığında turuncu gibi.
+                        Problem Açıklaması:
+        ·
+        • Kullanıcı iki renk girecek.
+        • Program bu iki rengin karışımını hesaplayacak.
+        • Eğer geçerli olmayan bir renk
+                girilirse hata mesajı girecek*/
+
+        Scanner input= new Scanner(System.in);
+        System.out.println("lütfen karışımını görmek istediğiniz iki rengi yazınız.\n kırmızı \n mavi \n sarı");
+        String renk1= input.next();
+        String renk2= input.next();
+
+
+        if ((renk1.toLowerCase().equals("sarı")&& renk2.toLowerCase().equals("mavi"))|| (renk1.toLowerCase().equals("mavi")&& renk2.toLowerCase().equals("sarı"))){
+
+            System.out.println("sarı + mavi = yeşil");
+
+        }else if ((renk1.toLowerCase().equals("sarı")&& renk2.toLowerCase().equals("kırmızı"))|| (renk1.toLowerCase().equals("kırmızı")&& renk2.toLowerCase().equals("sarı"))){
+
+            System.out.println("sarı + kırmızı = turuncu");
+
+        }else if ((renk1.toLowerCase().equals("kırmızı")&& renk2.toLowerCase().equals("mavi"))|| (renk1.toLowerCase().equals("mavi")&& renk2.toLowerCase().equals("kırmızı"))){
+
+            System.out.println("kırmızı + mavi = mor");
+
+        } else {
+            System.out.println("lütfen geçerli bir renk giriniz");
+        }
+        System.out.println("----------");
+
+
+
+
+
+    }
+}
